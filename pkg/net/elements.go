cbb6ffdb8a0663490845dea596ab93a98c9b81bc
Lulu-fw01 <markov.lu@outlook.com>
2024-02-17 22:17:43 +0300

    updated elemnts

--- pkg/net/elements.go
+++ pkg/net/elements.go
@@ -1,20 +1,31 @@
 package net
 
+import "encoding/xml"
+
 type Place struct {
-	id string
+	XMLName xml.Name `xml:"place"`
+	Id      string   `xml:"id,attr"`
 }
 
 type Transition struct {
-	id string
+	XMLName xml.Name `xml:"transition"`
+	Id      string   `xml:"id,attr"`
 }
 
 type Arc struct {
-	source string
-	target string
+	XMLName xml.Name `xml:"arc"`
+	Source  string   `xml:"source,attr"`
+	Target  string   `xml:"target,attr"`
 }
 
 type PetriNet struct {
-	places      []Place
-	transitions []Transition
-	arcs        []Arc
+	XMLName     xml.Name      `xml:"net"`
+	Places      []*Place      `xml:"place"`
+	Transitions []*Transition `xml:"transition"`
+	Arcs        []*Arc        `xml:"arc"`
+}
+
+type Pnml struct {
+	XMLName xml.Name  `xml:"pnml"`
+	Net     *PetriNet `xml:"net"`
 }
