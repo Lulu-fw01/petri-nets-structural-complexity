890ee77b8dd9a17577440c6ba6912d404477d173
Lulu-fw01 <markov.lu@outlook.com>
2024-03-02 01:11:23 +0300

    fixed net elements

--- pkg/net/elements.go
+++ pkg/net/elements.go
@@ -1,31 +1,20 @@
 package net
 
-import "encoding/xml"
-
 type Place struct {
-	XmlName xml.Name `xml:"place"`
-	Id      string   `xml:"id,attr"`
+	Id string
 }
 
 type Transition struct {
-	XMLName xml.Name `xml:"transition"`
-	Id      string   `xml:"id,attr"`
+	Id string
 }
 
 type Arc struct {
-	XMLName xml.Name `xml:"arc"`
-	Source  string   `xml:"source,attr"`
-	Target  string   `xml:"target,attr"`
+	Source string
+	Target string
 }
 
 type PetriNet struct {
-	XMLName     xml.Name      `xml:"net"`
-	Places      []*Place      `xml:"place"`
-	Transitions []*Transition `xml:"transition"`
-	Arcs        []*Arc        `xml:"arc"`
-}
-
-type Pnml struct {
-	XMLName xml.Name  `xml:"pnml"`
-	Net     *PetriNet `xml:"net"`
+	Places      []*Place
+	Transitions []*Transition
+	Arcs        []*Arc
 }
