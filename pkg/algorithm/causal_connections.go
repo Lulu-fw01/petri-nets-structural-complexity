683f875caf1653ef9eb64f660c9336c120707f94
Lulu-fw01 <markov.lu@outlook.com>
2024-03-10 20:16:32 +0300

    wip

--- pkg/algorithm/causal_connections.go
+++ pkg/algorithm/causal_connections.go
@@ -7,6 +7,10 @@ import (
 
 func FindCausalConnections(net *net.PetriNet, settings *settings.Settings) {
 	transitionToAgent := getTransitionToAgentMap(settings)
+	placesById := getPlacesMap(net.Places)
+	transitionById := getTransitionsMap(net.Transitions)
+	startPlaces := getStartPlaces(placesById, net.Arcs)
+	transitionsInputArcsCount := getTransitionsInputArcsCount(net.Arcs, transitionById)
 
 }
 
@@ -20,9 +24,43 @@ func getTransitionToAgentMap(settings *settings.Settings) map[string]string {
 	return transitionToAgent
 }
 
-// TODO rename.
-func CountCausalConnectionsMetric(net *net.PetriNet) {
+func getTransitionsInputArcsCount(arcs []*net.Arc, transitionsById map[string]*net.Transition) map[string]int {
+	transitionToInputArcsCount := make(map[string]int)
+	for _, arc := range arcs {
+		in := arc.Target
+		// If in element is transition.
+		if _, exists := transitionsById[in]; exists {
+			transitionToInputArcsCount[in]++
+		}
+	}
+	return transitionToInputArcsCount
+}
 
+func getTransitionsMap(transitions []*net.Transition) map[string]*net.Transition {
+	transitionsById := make(map[string]*net.Transition)
+	for _, t := range transitions {
+		transitionsById[t.Id] = t
+	}
+	return transitionsById
+}
+
+func getPlacesMap(places []*net.Place) map[string]*net.Place {
+	placesById := make(map[string]*net.Place)
+	for _, p := range places {
+		placesById[p.Id] = p
+	}
+	return placesById
+}
+
+func getStartPlaces(placesById map[string]*net.Place, arcs []*net.Arc) []string {
+	for _, arc := range arcs {
+		delete(placesById, arc.Target)
+	}
+	places := make([]string, 0, len(placesById))
+	for k := range placesById {
+		places = append(places, k)
+	}
+	return places
 }
 
 // result:
