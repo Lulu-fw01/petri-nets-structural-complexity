56b0e501e6a4c2f0cb56b2f9d158fc25dd160a09
Lulu-fw01 <markov.lu@outlook.com>
2024-03-30 14:54:26 +0300

    new utils functions

--- pkg/algorithm/utils.go
+++ pkg/algorithm/utils.go
@@ -10,3 +10,27 @@ func getPlacesMap(places []*net.Place) map[string]*net.Place {
 	}
 	return placesById
 }
+
+func getTransitionsMap(transitions []*net.Transition) map[string]*net.Transition {
+	transitionsById := make(map[string]*net.Transition)
+	for _, t := range transitions {
+		transitionsById[t.Id] = t
+	}
+	return transitionsById
+}
+
+// getInputAndOutputArcs returns two maps. The first map contains elements as keys and slices of arcs that end at these elements as values.
+// The second map contains elements as keys and slices of arcs that start at these elements as values.
+func getInputAndOutputArcs(arcs []*net.Arc) (map[string][]*net.Arc, map[string][]*net.Arc) {
+	elementToOutputArcs := make(map[string][]*net.Arc)
+	elementToInputArcs := make(map[string][]*net.Arc)
+
+	for _, arc := range arcs {
+		from := arc.Source
+		to := arc.Target
+
+		elementToOutputArcs[from] = append(elementToOutputArcs[from], arc)
+		elementToOutputArcs[to] = append(elementToInputArcs[to], arc)
+	}
+	return elementToInputArcs, elementToOutputArcs
+}
