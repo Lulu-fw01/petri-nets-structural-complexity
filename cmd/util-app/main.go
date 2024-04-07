0c42f0fb6519c46906d4befbb582bdd7aacc8141
Lulu-fw01 <markov.lu@outlook.com>
2024-04-08 01:50:48 +0300

    wip metric

--- cmd/util-app/main.go
+++ cmd/util-app/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	metric := flag.String("metrics", "all", "metric version")
+	settingsPath := flag.String("settings", "", "net settings")
+	netPath := flag.String("net", "", "net description")
+	flag.Parse()
+
+	if *settingsPath == "" {
+		fmt.Println("Please provide path to net settings.")
+		flag.Usage()
+		return
+	}
+
+	if *netPath == "" {
+		fmt.Println("Please provide path to net description.")
+		flag.Usage()
+		return
+	}
+
+	switch *metric {
+	case "all":
+		return
+	case "v1":
+	case "v2":
+	default:
+		println("Incorrect metric type.")
+		return
+	}
+
+	fmt.Printf("metric %s \nnet: %s \nsettings: %s\n", *metric, *netPath, *settingsPath)
 }
