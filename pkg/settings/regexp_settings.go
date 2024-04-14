e80efab5ca41c746c8c14464b8c740a4d35e23fc
Lulu-fw01 <markov.lu@outlook.com>
2024-04-14 15:42:55 +0300

    implemented settings interface for regexp settings

--- pkg/settings/regexp_settings.go
+++ pkg/settings/regexp_settings.go
@@ -1,1 +1,40 @@
 package settings
+
+import (
+	"complexity/pkg/net"
+	"fmt"
+	"regexp"
+)
+
+type RegexpSettings struct {
+	AgentsToTransitionRegexp map[string]string `json:"agentToTransitionRegexp"`
+	SilentTransitionsRegexp  string            `json:"silentTransitionRegexp"`
+}
+
+func (r RegexpSettings) GetTransitionAgent(transition *net.Transition) (*string, error) {
+	for agent, regexpStr := range r.AgentsToTransitionRegexp {
+		match, _ := regexp.MatchString(regexpStr, transition.Id)
+		if match {
+			return &agent, nil
+		}
+	}
+	return nil, fmt.Errorf("there are no agent  for transition with id %s", transition.Id)
+}
+
+func (r RegexpSettings) GetTransitionToAgentMap(transitions []*net.Transition) map[string]string {
+	result := make(map[string]string)
+	for _, t := range transitions {
+		for agent, regexpStr := range r.AgentsToTransitionRegexp {
+			match, _ := regexp.MatchString(regexpStr, t.Id)
+			if match {
+				result[agent] = t.Id
+			}
+		}
+	}
+	return result
+}
+
+func (r RegexpSettings) IsSilentTransition(transitionId string) bool {
+	match, _ := regexp.MatchString(r.SilentTransitionsRegexp, transitionId)
+	return match
+}
