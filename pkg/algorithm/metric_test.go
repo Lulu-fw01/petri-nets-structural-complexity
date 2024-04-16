47a4cf8123fd0682b843f27919c7d655dcb1894b
Lulu-fw01 <markov.lu@outlook.com>
2024-04-16 18:20:28 +0300

    test + metric fix

--- pkg/algorithm/metric_test.go
+++ pkg/algorithm/metric_test.go
@@ -9,80 +9,52 @@ import (
 func TestCountRatiosFor2AgentsHappyPath(t *testing.T) {
 	netSettings, newNet := testUtils.ReadSettingsAndNet(t, "testdata/2-agents-settings.json", "testdata/2-agents.xml")
 
-	result := CountRatios(newNet, netSettings)
+	result := CountMetricVersion1(newNet, netSettings)
 
-	if len(result) != 1 {
-		t.Fatalf("Expecte size of result list 1, actual: %d", len(result))
-	}
-
-	firstMetric := result[0]
-	assertions.AssertMetric(t, 0.625, firstMetric.ratio)
+	assertions.AssertMetric(t, 0.625, result)
 }
 
 // 2 agents, no connections.
 func TestCountMetricForNetWithNoChannels(t *testing.T) {
 	netSettings, newNet := testUtils.ReadSettingsAndNet(t, "testdata/common-settings.json", "testdata/no-channels-net.xml")
 
-	result := CountRatios(newNet, netSettings)
+	result := CountMetricVersion1(newNet, netSettings)
 
-	// if there are no connections between 2 list will be empty.
-	if len(result) != 0 {
-		t.Fatalf("Expecte size of result list 0, actual: %d", len(result))
-	}
+	assertions.AssertMetric(t, 1.0, result)
 }
 
 // 2 agents^ 1 channel, 1 connection.
 func TestCountMetricForNetWith1ConnectionBetweenAgents(t *testing.T) {
 	netSettings, newNet := testUtils.ReadSettingsAndNet(t, "testdata/common-settings.json", "testdata/2-agents-v2.xml")
 
-	result := CountRatios(newNet, netSettings)
+	result := CountMetricVersion1(newNet, netSettings)
 
-	if len(result) != 1 {
-		t.Fatalf("Expecte size of result list 1, actual: %d", len(result))
-	}
-
-	firstMetric := result[0]
-	assertions.AssertMetric(t, 0.8, firstMetric.ratio)
+	assertions.AssertMetric(t, 0.8, result)
 }
 
 // 2 agents, 1 channel, 2 connections.
 func TestCountMetricForNetWith2ConnectionsBetweenAgents(t *testing.T) {
 	netSettings, newNet := testUtils.ReadSettingsAndNet(t, "testdata/common-settings.json", "testdata/2-agents-v3.xml")
 
-	result := CountRatios(newNet, netSettings)
+	result := CountMetricVersion1(newNet, netSettings)
 
-	if len(result) != 1 {
-		t.Fatalf("Expecte size of result list 1, actual: %d", len(result))
-	}
-
-	firstMetric := result[0]
-	assertions.AssertMetric(t, 0.666667, firstMetric.ratio)
+	assertions.AssertMetric(t, 0.666667, result)
 }
 
 // 2 agents, 2 channels, 2 and 2 connections.
 func TestCountMetricForNetWith4ConnectionsBetweenAgentsAnd2Channels(t *testing.T) {
 	netSettings, newNet := testUtils.ReadSettingsAndNet(t, "testdata/common-settings.json", "testdata/2-agents-v4.xml")
 
-	result := CountRatios(newNet, netSettings)
+	result := CountMetricVersion1(newNet, netSettings)
 
-	if len(result) != 1 {
-		t.Fatalf("Expecte size of result list 1, actual: %d", len(result))
-	}
-
-	firstMetric := result[0]
-	assertions.AssertMetric(t, 0.571429, firstMetric.ratio)
+	assertions.AssertMetric(t, 0.571429, result)
 }
 
 // 2 agents, 2 channels, 2 and 4 connections.
 func TestCountMetricForNetWith5ConnectionsBetweenAgentsAnd2Channels(t *testing.T) {
 	netSettings, newNet := testUtils.ReadSettingsAndNet(t, "testdata/common-settings.json", "testdata/2-agents-v5.xml")
 
-	result := CountRatios(newNet, netSettings)
+	result := CountMetricVersion1(newNet, netSettings)
 
-	if len(result) != 1 {
-		t.Fatalf("Expecte size of result list 1, actual: %d", len(result))
-	}
-
-	firstMetric := result[0]
-	assertions.AssertMetric(t, 0.5, firstMetric.ratio)
+	assertions.AssertMetric(t, 0.5, result)
 }
