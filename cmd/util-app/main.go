26d9e578f23eb8a18860da9288cb12c4a754d11d
Lulu-fw01 <markov.lu@outlook.com>
2024-04-30 13:43:57 +0300

    wip

--- cmd/util-app/main.go
+++ cmd/util-app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"complexity/internal/reader"
 	"complexity/internal/reader/pipe"
+	"complexity/internal/writer"
 	"complexity/pkg/algorithm"
 	"complexity/pkg/net"
 	"complexity/pkg/settings"
@@ -17,6 +18,7 @@ const (
 	SettingsTypeFlag   = "settings-type"
 	SettingsPathFlag   = "settings"
 	NetPathFlag        = "net"
+	FileOutputFlag     = "file"
 	SimpleSettingsType = "simple"
 	RegexpSettingsType = "regexp"
 	AllMetricType      = "all"
@@ -30,6 +32,7 @@ func main() {
 	settingsType := flag.String(SettingsTypeFlag, SimpleSettingsType, "settings type (simple or regexp)")
 	settingsPath := flag.String(SettingsPathFlag, "", "net settings")
 	netPath := flag.String(NetPathFlag, "", "net description")
+	filePath := flag.String(FileOutputFlag, "", "path to output file")
 	flag.Parse()
 
 	validateSettingsPath(*settingsPath)
@@ -41,14 +44,16 @@ func main() {
 		return
 	}
 
+	output := getOutputFunction(*filePath)
+
 	if *isBatchProcess {
-		batchFlow(*netPath, *metric, netSettings)
+		batchFlow(*netPath, *metric, netSettings, output)
 	} else {
-		standardFlow(*netPath, *metric, netSettings)
+		standardFlow(*netPath, *metric, netSettings, output)
 	}
 }
 
-func standardFlow(netPath, metric string, netSettings settings.Settings) {
+func standardFlow(netPath, metric string, netSettings settings.Settings, fn writer.OutputFunc) {
 	netToProcess, err := reader.ReadNet[pipe.Pnml](netPath, netSettings)
 	if err != nil {
 		fmt.Printf("Erorr: %s", err)
@@ -69,7 +74,11 @@ func standardFlow(netPath, metric string, netSettings settings.Settings) {
 	}
 }
 
-func batchFlow(dirPath, metric string, netSettings settings.Settings) {
+func countCharacteristicsAndGetMessage(net *net.PetriNet, metric string, netSettings settings.Settings) string {
+
+}
+
+func batchFlow(dirPath, metric string, netSettings settings.Settings, fn writer.OutputFunc) {
 	files, err := os.ReadDir(dirPath)
 	if err != nil {
 		fmt.Println("Error reading directory:", err)
@@ -122,4 +131,40 @@ func printMetricV1(net *net.PetriNet, settings settings.Settings) {
 func printMetricV2(net *net.PetriNet, settings settings.Settings) {
 	metric := algorithm.CountMetric(net, settings)
 	fmt.Printf("Metric 2 equals %f\n", metric)
+}
+
+func getCharacteristicV1Message(value float64) string {
+	return fmt.Sprintf("Characteristic 1 equals %f\n", value)
+}
+
+func getCharacteristicV2Message(value float64) string {
+	return fmt.Sprintf("Characteristic 2 equals %f\n", value)
+
+}
+
+func getOutputFunction(filePath string) writer.OutputFunc {
+	if filePath == "" {
+		return consoleOutput
+	}
+	return func(text string) {
+		f, err := os.Create(filePath)
+		if err != nil {
+			// todo add error to message.
+			panic("Can't write to file")
+		}
+		defer f.Close()
+
+		data := []byte(text)
+
+		_, err = f.Write(data)
+
+		if err != nil {
+			// todo add error message.
+			panic("Error writing to file")
+		}
+	}
+}
+
+func consoleOutput(text string) {
+	fmt.Printf(text)
 }
