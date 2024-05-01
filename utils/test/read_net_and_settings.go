72d9b1b7aa5136e6bf2c500e170dec8e0935236f
Lulu-fw01 <markov.lu@outlook.com>
2024-05-01 23:34:39 +0300

    new utils

--- utils/test/read_net_and_settings.go
+++ utils/test/read_net_and_settings.go
@@ -19,3 +19,19 @@ func ReadSettingsAndNet(t *testing.T, settingsPath, netPath string) (settings.Se
 	}
 	return netSettings, newNet
 }
+
+func ReadSettings[S settings.Settings](t *testing.T, settingsPath string) settings.Settings {
+	netSettings, err := settings.ReadSettings[S](settingsPath)
+	if err != nil {
+		t.Fatalf("Error reading settings from %s. err: %s", settingsPath, err)
+	}
+	return *netSettings
+}
+
+func ReadNet(t *testing.T, netSettings settings.Settings, netPath string) *net.PetriNet {
+	newNet, err := reader.ReadNet[pipe.Pnml](netPath, netSettings)
+	if err != nil {
+		t.Fatalf("Error reading net from %s. err: %s", netPath, err)
+	}
+	return newNet
+}
