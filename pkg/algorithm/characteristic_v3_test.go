18275909d0ab9d152db5a1f90c3196a6e7a3e96b
Lulu-fw01 <markov.lu@outlook.com>
2024-05-11 18:22:14 +0300

    test for external behavior

--- pkg/algorithm/characteristic_v3_test.go
+++ pkg/algorithm/characteristic_v3_test.go
@@ -102,3 +102,46 @@ func TestCountCharacteristicV3InternalBehaviorWeights(t *testing.T) {
 		})
 	}
 }
+
+func TestCountCharacteristicV3ExternalBehaviorWeights(t *testing.T) {
+	commonSettings := testUtils.ReadSettings[settings.SimpleSettings](t, "testdata/external-behavior-test/common-with-external-behavior.json")
+
+	tests := []struct {
+		name string
+		args CharacteristicArgs
+		want float64
+	}{
+		{
+			name: "no channels test",
+			args: GetArgs(t, commonSettings, "testdata/no-channels-net.xml"),
+			want: 1.0,
+		},
+		{
+			name: "1 connection 1 channel",
+			args: GetArgs(t, commonSettings, "testdata/2-agents-v2.xml"),
+			want: 0.833333333333,
+		},
+		{
+			name: "2 agents, 1 channel, 2 connections test",
+			args: GetArgs(t, commonSettings, "testdata/2-agents-v3.xml"),
+			want: 0.7777777777777,
+		},
+		{
+			name: "2 agents, 2 channels, 2 and 2 connections test",
+			args: GetArgs(t, commonSettings, "testdata/2-agents-v4.xml"),
+			want: 0.2777777777777,
+		},
+		{
+			name: "2 agents, 2 channels, 2 and 4 connections test",
+			args: GetArgs(t, commonSettings, "testdata/2-agents-v5.xml"),
+			want: 0.2222222222222,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CountCharacteristicV3(tt.args.net, tt.args.settings); !assertions.IsCorrect(tt.want, got) {
+				t.Errorf("CountCharacteristicV3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
